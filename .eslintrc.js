9fb9fdc46d48e6272f06f0f6ac309200ce8fc31e
Abdelmajid habouch <habush1610@gmail.com>
2025-03-13 13:39:39 +0000

    Fix 4.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,49 @@
-export default [
-  {
-    'env': {
-      'node': true,
-      'es6': true,
-      'jest/globals': true
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true,
+    'jest/globals': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    'extends': [
-      'eslint:recommended',
-      'plugin:react/recommended'
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react', 'jest'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    'parserOptions': {
-      'ecmaFeatures': {
-        'jsx': true
-      },
-      'ecmaVersion': 2018,
-      'sourceType': 'module'
-    },
-    'plugins': [
-      'react', 'jest'
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'rules': {
-      'indent': [
-        'error',
-        2
-      ],
-      'linebreak-style': [
-        'error',
-        'unix'
-      ],
-      'quotes': [
-        'error',
-        'single'
-      ],
-      'semi': [
-        'error',
-        'never'
-      ],
-      'eqeqeq': 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': [
-        'error', 'always'
-      ],
-      'arrow-spacing': [
-        'error', { 'before': true, 'after': true }
-      ],
-      'no-console': 'off',
-      'react/prop-types': 0
-    }
-  }  
-]
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 'off',
+    'react/prop-types': 0
+  }
+}
