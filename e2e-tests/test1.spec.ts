201cd9a5f2703a7a4bd50867d3804d7e4c1f95a0
Abdelmajid habouch <habush1610@gmail.com>
2025-03-14 11:58:16 +0000

    Add a test for visiting a pokemon page

--- e2e-tests/test1.spec.ts
+++ e2e-tests/test1.spec.ts
@@ -5,3 +5,9 @@ test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
 })
+
+test('can visit a pokemon page', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+})
